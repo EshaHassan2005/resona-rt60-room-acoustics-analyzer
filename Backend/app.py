ea60beb7cc032091dacb354196117b50bfcaba5d
Azrin Karim <azrinkarim70@gmail.com>
2026-08-10 17:26:13 +0600

    made a few changes

--- Backend/app.py
+++ Backend/app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask,request,jsonify
+
+import librosa
 
 
 app=Flask(__name__)
@@ -10,5 +12,20 @@ app=Flask(__name__)
 def index():
     return "Hello world"
 
+#sending a wav file to backend and recieve info back
+
+@app.route('/upload',methods=['POST','GET'])
+def upload():
+    if 'audio' not in request.files:
+        return jsonify({"error" : "No audio file was uploaded"}), 400
+
+    file=request.files['audio']
+
+    if file.filename=='':
+        return jsonify({"error":"File was not found"}),404
+
+    spec=librosa.load()
+
+
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=True) 
