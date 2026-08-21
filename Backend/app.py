94d903bb012d72b3a01033f84c30e5f0b1cf62f6
Azrin Karim <azrinkarim70@gmail.com>
2026-08-21 22:16:41 +0600

    added RT60

--- Backend/app.py
+++ Backend/app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 
 from audio_utils import load_audio, downsample_for_preview, AudioLoadError
 from dsp import energy_decay, get_fft, all_octave_bands
+from scipy import stats
 
 import numpy as np
 
@@ -111,6 +112,48 @@ def fft_route():
         "amplitude": amp[:half].tolist(),
         "frequencies": freq[:half].tolist(),
     }), 200
+
+
+#RT60 calculation part
+@app.route('/RT60',method=['GET','POST'])
+def rt60_route():
+    try:
+        signal, sample_rate=get_uploaded_audio()
+        decay_db_y=energy_decay(signal, sample_rate)
+    except AudioLoadError as e:
+        return jsonify({"error":str(e)}), 400
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 422
+    
+    time_x = (np.arange(len(decay_db_y))/ sample_rate)
+    #for T20
+    index_y_start=np.abs(decay_db_y-(-5)).argmin()
+    index_y_end=np.abs(decay_db_y-(-25)).argmin()
+
+    y=decay_db_y[index_y_start:index_y_end+1]
+    x=time_x[index_y_start:index_y_end+1]
+
+    slope, intercept, r, p, std_err = stats.linregress(x.tolist(),y.tolist())
+
+    RT60_20=-(60/slope) #####
+
+
+    #for T30
+
+    index_y_start=np.abs(decay_db_y-(-5)).argmin()
+    index_y_end=np.abs(decay_db_y-(-35)).argmin()
+
+    y=decay_db_y[index_y_start:index_y_end+1]
+    x=time_x[index_y_start:index_y_end+1]
+
+    slope, intercept, r, p, std_err = stats.linregress(x.tolist(),y.tolist())
+
+    RT60_30=-(60/slope) #####
+
+    return jsonify({"status":"success","RT60_20":RT60_20,"RT60_30":RT60_30}),200
+
+
+    
 
 if __name__=="__main__":
     app.run(debug=True) 
